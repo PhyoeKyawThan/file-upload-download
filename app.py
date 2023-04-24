08db3528058c284e53fa9f53429f0f409537e7d8
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2023-04-25 01:06:05 +0630

    comlete download feature

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ def index():
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        filename = secure_filename(file.filename)
-        if filename not in os.listdir('files'):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'file uploaded'
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            if filename not in os.listdir('files'):
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return render_template('index.html', success='Successfully File Uploaded')
+            else:
+                return render_template('index.html', msg = 'File already Exists')
         else:
-            return render_template('index.html', msg = 'File already Exists')
+            return render_template('upload.html', msg='File must not be empty to upload')
     else:
         return render_template('upload.html')
 
@@ -27,6 +30,13 @@ def view():
     data_list = os.listdir('files')
     return render_template('view.html', files=data_list)    
     
+@app.route('/download/<filename>', methods=['POST', 'GET'])
+def download(filename):
+    if request.method == 'GET':
+        download_path = f'files/{filename}'
+        return send_file(download_path, as_attachment=True)
+    else:
+        return render_template('view.html')
 
 
 if __name__=='__main__':
