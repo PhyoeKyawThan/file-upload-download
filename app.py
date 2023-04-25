67c53b4017c162770b7b76bc8883f533992cfbe4
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2023-04-25 08:45:56 +0630

    file name input updated

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 import os
+from db import db
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'files'
+
+db = db.DB()
 
 @app.route('/')
 def index():
@@ -13,10 +16,17 @@ def index():
 def upload():
     if request.method == 'POST':
         file = request.files['file']
+        org_filename, fileExten = os.path.splitext(file.filename)
+        _filename = request.form['filename']
         if file.filename != '':
-            filename = secure_filename(file.filename)
+            filename = f'{secure_filename(_filename)}{fileExten}'
             if filename not in os.listdir('files'):
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                data = {
+                    'fileName': secure_filename(_filename),
+                    'filePath': os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                }
+                db.insert(data)
                 return render_template('index.html', success='Successfully File Uploaded')
             else:
                 return render_template('index.html', msg = 'File already Exists')
@@ -38,6 +48,10 @@ def download(filename):
     else:
         return render_template('view.html')
 
+@app.route('/preview/<filename>')
+def preview(filename):
+    path = f'files/{filename}'
+    return send_file(path, as_attachment=False)
 
 if __name__=='__main__':
     app.run(debug=True)
--- db/db.py
+++ db/db.py
@@ -5,7 +5,6 @@ class CreateDB():
         self.engine = create_engine("mysql://root:Domak9090@127.0.0.1:3306/file")
         metadata = MetaData()
         self.table_name = "fileData"
-        self.conn      = self.engine.connect()
         self.table = Table(self.table_name, metadata, 
                       Column('id', Integer, primary_key=True),
                       Column('fileName', String(20), nullable=False), 
@@ -14,19 +13,22 @@ class CreateDB():
 
 class DB(CreateDB):
     def insert(self, data: list):
+        self.conn      = self.engine.connect()
         self.conn.execute(self.table.insert(), data)
         self.conn.commit()
+        self.conn.close()
     
     def delete(self, id: int):
+        self.conn      = self.engine.connect()
         self.conn.execute(self.table.delete().where(self.table.c.id == id))
         self.conn.commit()
-
-    def show(self):
-        return self.conn.execute(self.table.select())
-
-    def __del__(self):
         self.conn.close()
 
+    def show(self):
+        self.conn      = self.engine.connect()
+        return self.conn.execute(self.table.select())
+    
+        
 if __name__=='__main__':
     db = DB()
     db.insert([{'fileName': 'python', 'filePath': '/dir/test/test.pdf'}])
